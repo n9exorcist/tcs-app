108a7c9e72f2d6a50a490b5bf195b9bd3ffa5376
n9exorcist <n9exorcist@gmail.com>
2019-07-27 20:21:15 +0530

    Usage of Subject and Observable

--- src/app/sortable/soratable-column.service.ts
+++ src/app/sortable/soratable-column.service.ts
@@ -8,12 +8,15 @@ export class SoratableColumnService {
 
   constructor() { }
 
+  // Subject allows you to both publish and subscribe
   private columnSortedSource = new Subject<ColumnSortedEvent>();
 
   columnSorted$ = this.columnSortedSource.asObservable();
 
   columnSorted(event:ColumnSortedEvent){
+    // The subject next method is used to send messages to an observable which are then sent to all angular components that are subscribers (a.k.a. observers) of that observable.
     this.columnSortedSource.next(event);
+    console.log("1. The subject next method is used to send messages to an observable");
   }
 }
 
--- src/app/sortable/sortable.component.ts
+++ src/app/sortable/sortable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Component, OnInit, Input, HostListener, OnDestroy } from '@angular/core';
 import { SoratableColumnService } from './soratable-column.service';
 import { Subscription } from 'rxjs';
 
@@ -7,17 +7,23 @@ import { Subscription } from 'rxjs';
   templateUrl: './sortable.component.html',
   styleUrls: ['./sortable.component.css']
 })
-export class SortableComponent implements OnInit {
+export class SortableComponent implements OnInit, OnDestroy {
 
   constructor(private SoratableColumnService: SoratableColumnService ) { }
 
   @Input('sortable-column') columnName: string;
   @Input('sortable-direction') sortDirection: string = '';
+  // Component that Receives Messages
   public columnSortedSubscription: Subscription;
 
   ngOnInit() {
+    // subscribe to sort changes so we can react when other columns are sorted
+    // The observable subscribe method is used by angular components to subscribe to messages that are sent to an observable.
     this.columnSortedSubscription = this.SoratableColumnService.columnSorted$.subscribe(event => {
+      // reset this column's sort direction to hide the sort icons
+      console.log("event", event);
       if(this.columnName != event.sortColumn){
+        console.log("subscribe to sort changes so we can react when other columns are sorted");
         this.sortDirection = '';
       }
     })
@@ -27,6 +33,11 @@ export class SortableComponent implements OnInit {
   @HostListener('click') sort() {
     this.sortDirection = this.sortDirection === 'asc'? 'desc' : 'asc';
     this.SoratableColumnService.columnSorted({sortColumn: this.columnName, sortDirection:this.sortDirection });
-    console.log("Host Listener is working");
+    console.log("2. Host Listener is working");
+  }
+
+  ngOnDestroy(){
+    this.columnSortedSubscription.unsubscribe();
+    console.log("3. Destroyed");
   }
 }
